7a15669834bbf78232fd7daa352fd2464c981d2d
alexseva94 <alexseva94@gmail.com>
2025-06-09 19:50:09 +0300

    17-add-api-key-protection

--- src/constants/error-message.ts
+++ src/constants/error-message.ts
@@ -2,4 +2,6 @@ export const errorMessage = {
     missingEnvironmentVariable: "Missing environment variable {0}",
     internalServerError: "Internal server error",
     resourceNotFound: "Resource {0} with id {1} not found",
+    apiKeyMissing: "API key is missing",
+    apiKeyInvalid: "API key is invalid",
 } as const;
